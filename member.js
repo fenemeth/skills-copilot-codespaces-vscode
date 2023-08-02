8ca0d1ac9a1e54811b16524441a85373aff350b5
fenemeth <69605829+fenemeth@users.noreply.github.com>
2023-08-02 01:31:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  let member = document.getElementById("member");
+  let skills = document.getElementById("skills");
+  let projects = document.getElementById("projects");
+  let contact = document.getElementById("contact");
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+}
